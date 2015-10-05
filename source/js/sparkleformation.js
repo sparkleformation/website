6585e02cea6b63d90de5ee65cd533f2fb8b680ab
Caleb Hailey <calebhailey@gmail.com>
2015-10-05 10:21:16 -0700

    derpy commit of changes to animate example sfn output

--- source/js/sparkleformation.js
+++ source/js/sparkleformation.js
@@ -9,4 +9,23 @@ $(document).ready(function() {
   $('.coming-soon a').click(false);
   $('.coming-soon a').html("Coming soon >");
 
+  $('#sfn-demo code').focus()
+  .typetype( $('#demo-1-input').val(), {e:0});
+  standardOut( '#demo-1-prompts', '#sfn-demo code', 5000,  500)
+  standardOut( '#demo-1-events',  '#sfn-demo code', 10000,  2500 )
+  standardOut( '#demo-1-output',  '#sfn-demo code', 18000, 0 )
 });
+
+function standardOut (output, target, start, interval) {
+  var delay = start
+  var line = 0
+  var lines = ( $(output).val().split("\n").length )
+  for (count = 0; count < lines; count++) {
+    setTimeout(function(){
+      console.log( $(output).val().split("\n")[line] )
+      $(target).append( "\n" + $(output).val().split("\n")[line] );
+      line += 1;
+    }, delay);
+    delay += interval;
+  };
+};
