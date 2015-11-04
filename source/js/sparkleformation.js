4fc3b1b21e4ef12506bd3f92ab57f3183a1dc3c2
Caleb Hailey <calebhailey@gmail.com>
2015-11-04 15:09:54 -0800

    move sfn demo js to separate file so it will only be loaded on the home page

--- source/js/sparkleformation.js
+++ source/js/sparkleformation.js
@@ -10,23 +10,4 @@ $(document).ready(function() {
   $('.coming-soon a').html("Coming soon >");
 
   $('.documentation-content table').addClass('table table-hover table-condensed table-responsive')
-
-  $('#sfn-demo code').focus()
-  .typetype( $('#demo-1-input').val(), {e:0});
-  stdOut( '#demo-1-prompts', '#sfn-demo code', 5000,  500);
-  stdOut( '#demo-1-events',  '#sfn-demo code', 10000,  2500 );
-  stdOut( '#demo-1-output',  '#sfn-demo code', 18000, 0 );
 });
-
-function stdOut (output, target, start, interval) {
-  var delay = start;
-  var line = 0;
-  var lines = ( $(output).val().split("\n").length );
-  for (count = 0; count < lines; count++) {
-    setTimeout(function(){
-      $(target).append( "\n" + $(output).val().split("\n")[line] );
-      line += 1;
-    }, delay);
-    delay += interval;
-  };
-};
